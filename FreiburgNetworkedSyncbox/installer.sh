78229d281d168673700cd966286040026f876f7d
James Bruska <jamesbthecoder@gmail.com>
2023-03-01 12:41:06 -0500

    Added installer for both syncboxes. This takes care of the libusb and liblabjackusb dependencies

---
+++ FreiburgNetworkedSyncbox/installer.sh
@@ -0,0 +1,22 @@
+#! /bin/bash
+
+# Make build directory
+mkdir build
+cd build
+
+# Install libusb-1.0.26
+if [ ! -d "libusb-1.0.26" ]; then
+    curl -OL https://github.com/libusb/libusb/releases/download/v1.0.26/libusb-1.0.26.tar.bz2
+    tar xvfj libusb-1.0.26.tar.bz2 
+    cd libusb-1.0.26/
+    ./configure
+    make
+    make check
+    sudo make install
+    cd ..
+    rm libusb-1.0.26.tar.bz2
+fi
+
+# Make code
+cmake ..
+make -j
